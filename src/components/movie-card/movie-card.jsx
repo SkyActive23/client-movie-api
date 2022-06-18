c3ab0b3d31d258cafee2ddf25002a86a23b67b40
Sky <skyrj50031@maricopa.edu>
2022-06-17 21:45:54 -0700

    Update movie-card.jsx
    testing new page

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -17,5 +17,5 @@ MovieCard.propTypes = {
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequire
 };
