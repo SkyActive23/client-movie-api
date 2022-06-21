d854a98c97ac7d0012c35eb1cce97af5f84eca2c
Sky <skyrj50031@maricopa.edu>
2022-06-21 14:20:41 -0700

    Update login-view.jsx
    Updated loginView  added bootstrap tools

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Nav, Navbar, Form, Button, Card, CardGroup, Container, Row, Col } from 'react-bootstrap';
+import './login-view.scss';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -15,19 +17,47 @@ export function LoginView(props) {
 
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
-  );
+        <Container className="Background">
+                <Navbar variant="dark" expand="lg" className="nav">
+                    <Container>
+                        <Navbar.Brand href="#home">ApiFlix</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#link">Link</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
+                <Row>
+                    <Col>
+                        <CardGroup className='LogCardGroup'>
+                            <Card>
+                                <Card.Body className="LogCard">
+                                    <Card.Title className='title'>Login Here</Card.Title>
+                                    <Form>
+                                        <Form.Group controlId="formUsername">
+                                            <Form.Label>Username:</Form.Label>
+                                            <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formPassword">
+                                            <Form.Label>Password:</Form.Label>
+                                            <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit" onClick={handleSubmit} className="button">
+                                            Submit
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                            </Card>
+                        </CardGroup>
+                    </Col>
+                </Row>
+            
+        </Container>
+    );
 }
