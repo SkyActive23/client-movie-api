d42f379dded1d7be3eb91f48b2a8c0f4ebd2aa9c
Sky <skyrj50031@maricopa.edu>
2022-06-30 23:13:27 -0700

    Create navbar-view.jsx
    3.6 NavbarView Page

---
+++ src/components/navbar-view/navbar-view.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import './navbar-view.scss';
+import { Navbar, Container, Nav, Button, Offcanvas, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+export function NavbarView({ user }) {
+
+    const onLoggedOut = () => {
+        localStorage.clear();
+        window.open("/", "_self");
+    }
+
+    const isAuth = () => {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken) {
+            return accessToken;
+        } else {
+            return false;
+        }
+    };
+
+    return (
+        <Container>
+            <Navbar bg="light" expand="lg">
+                <Container className="navbar-container">
+                    <Navbar.Brand as={Link} to={"/"} href="#home">ApiFlix</Navbar.Brand>
+
+                    <Nav className="me-auto navbar-elements__style">
+
+                        {isAuth() && (
+                            <Nav.Link as={Link} to={`/`}>Movies</Nav.Link>
+                        )}
+
+                        {isAuth() && (
+                            <Nav.Link as={Link} to={`/users/${user}`}>Profile</Nav.Link>
+                        )}
+
+                        {isAuth() && (
+                            <Nav.Link onClick={() => onLoggedOut()}>Logout</Nav.Link>
+                        )}
+
+                        {!isAuth() && (
+                            <Nav.Link as={Link} to={`/`}>Login</Nav.Link>
+                        )}
+
+                        {!isAuth() && (
+                            <Nav.Link as={Link} to={`/register`}>Sign Up</Nav.Link>
+                        )}
+
+
+                    </Nav>
+
+                </Container>
+            </Navbar>
+        </Container>
+
+    )
+}
