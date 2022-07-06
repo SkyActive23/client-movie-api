407ae7127e18badcd9afc400b2bc3a0bba63f178
Sky <skyrj50031@maricopa.edu>
2022-07-05 17:44:39 -0700

    Update movie-card.jsx
    fixed quotes in link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import axios from 'axios';
 
@@ -26,12 +26,12 @@ export class MovieCard extends React.Component {
     return (
       <Card className='MovieCard'>
         <Link>
-          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
         </Link>
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Link to='/movies/${movie._id}'>
+          <Link to={`/movies/${movie._id}`}>
             <Button variant='link'>Open</Button>
           </Link>
           <Button className="button ml-2" variant="outline-primary" size="sm" onClick={() => this.addToFavoriteList(movie._id) }>Add</Button>
@@ -41,17 +41,17 @@ export class MovieCard extends React.Component {
   }
 }
 
-// MovieCard.propTypes = {
-//   movie: PropTypes.shape({
-//     Title: PropTypes.string.isRequired,
-//     Description: PropTypes.string.isRequired,
-//     ImagePath: PropTypes.string.isRequired,
-//     Director: PropTypes.shape({
-//       Name: PropTypes.string.isRequired,
-//       Bio: PropTypes.string.isRequired,
-//     }),
-//     Genre: PropTypes.shape({
-//       Name: PropTypes.string.isRequired,
-//     })
-//   }).isRequired,
-// };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    // ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    })
+  }).isRequired,
+};
