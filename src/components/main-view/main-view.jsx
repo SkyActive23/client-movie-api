c86430224cb6dec858449427a7c39e77999f12e6
Sky <skyrj50031@maricopa.edu>
2022-07-05 17:44:05 -0700

    Update main-view.jsx
    imported redirect

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import './main-view.scss';
 import { NavbarView } from '../navbar-view/navbar-view'
