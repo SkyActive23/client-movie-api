1df0929e5c5ee14ea8bde6ad5e89ccc796db9bdd
Sky <skyrj50031@maricopa.edu>
2022-08-05 12:49:36 -0700

    Update movie-view.jsx
    Deleted old code

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -72,61 +72,6 @@ export class MovieView extends React.Component {
                     <Button variant="secondary" onClick={() => { onBackClick(null); }} className="back-button">Back</Button>
                 </Card.Body>
             </Card>
-
-            // <Card className="movie-view">
-            //     <Col className='movie-col'>
-            //         <div className="movie-info">
-            //             <Col>
-            //                 <Row>
-            //                     <div className="movie-poster">
-            //                         <img className='movie-img' variant="top" src={movie.ImageURL} />
-            //                     </div>
-            //                 </Row>
-            //             </Col>
-            //             <Col>
-            //                 <Row>
-            //                     <div className="movie-title">
-            //                         <span className="label">Title: </span>
-            //                         <span className="value">{movie.Title}</span>
-            //                     </div>
-            //                 </Row>
-            //                 <Row>
-            //                     <div className="movie-description">
-            //                         <span className="label">Description: </span>
-            //                         <span className="value">{movie.Description}</span>
-            //                     </div>
-            //                 </Row>
-            //                 <Row>
-            //                     <div className="movie-genre">
-            //                         <span className="label">Genre: </span>
-            //                         <span className="value">{movie.Genre.Name}</span>
-            //                     </div>
-            //                 </Row>
-            //                 <Row>
-            //                     <div className="movie-director">
-            //                         <span className="label">Director: </span>
-            //                         <span className="value">{movie.Director.Name}</span>
-            //                     </div>
-            //                 </Row>
-            //                 <Row>
-            //                     <div>
-            //                         <Link to={`/directors/${movie.Director.Name}`}>
-            //                             <Button className='director-button' variant="outline-primary">Director</Button>
-            //                         </Link>
-            //                     </div>
-            //                 </Row>
-            //                 <Row>
-            //                     <div className="director-bio">
-            //                         <span className="label">Bio: </span>
-            //                         <span className="value">{movie.Director.Bio}</span>
-            //                     </div>
-            //                 </Row>
-            //             </Col>
-            //         </div>
-            //         <Button className="add-button" id="favorites-btn" onClick={() => this.addToFavoriteList(movie._id)}>Add</Button>
-            //         <Button onClick={() => { onBackClick(null); }} className="back-button">Back</Button>
-            //     </Col>
-            // </Card>
         );
     }
 }
