2fc6edabfedf2d050d8d6babe197edec08dda0fa
Sky <skyrj50031@maricopa.edu>
2022-08-05 12:11:15 -0700

    Update movies-list.jsx
    added movie list to complete redux

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -6,29 +6,29 @@ import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-
 import { MovieCard } from '../movie-card/movie-card';
 
 const mapStateToProps = state => {
-    const { visibilityFilter } = state;
-    return { visibilityFilter };
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
 };
 
 function MoviesList(props) {
-    const { movies, visibilityFilter } = props;
-    let filteredMovies = movies;
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
 
-    if (visibilityFilter !== '') {
-        filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
-    }
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+  }
 
-    if (!movies) return <div className="main-view"/>;
+  if (!movies) return <div className="main-view"/>;
 
-    return <>
-        <Col md={12} style={{ margin: '1em' }}>
-            <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-        </Col>
-        {filteredMovies.map(m => (
-            <Col md={3} key={m._id}>
-                <MovieCard movie={m} />
-            </Col>
-        ))}
+  return <>
+    <Col md={12} style={{ margin: '1em' }}>
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+    </Col>
+    {filteredMovies.map(m => (
+      <Col md={3} key={m._id}>
+        <MovieCard movie={m} />
+      </Col>
+    ))}
   </>;
 }
 
